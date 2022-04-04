793210c27d07b5a3437473ca7a8c0f16670c0593
SandeepVerma3105 <sandeep.verma@appventurez.com>
2022-04-04 12:28:07 +0530

    bugs

--- router/index.js
+++ router/index.js
@@ -2,11 +2,11 @@ const express = require("express")
 const Router = express.Router()
 const merchantRouters = require("./merchant/index")
 const customersRouters = require("./user/index")
-const adminRouter = require("./admin/index")
+    // const adminRouter = require("./admin/index")
 Router.use(express.json())
 Router.use(express.urlencoded({ extended: false }))
 
 Router.use("/merchant", merchantRouters)
 Router.use("/customer", customersRouters)
-Router.use("/admin", adminRouter)
+    // Router.use("/admin", adminRouter)
 module.exports = Router
