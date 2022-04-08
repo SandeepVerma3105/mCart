44b7f8bc9fc5c25a4e8035143d5786c8e4ba19dd
SandeepVerma3105 <sandeep.verma@appventurez.com>
2022-04-08 17:48:57 +0530

    size block user

--- response/success.js
+++ response/success.js
@@ -1,8 +1,5 @@
 const successResponse = async(status, response = "null", code, error, message, data) => {
-    console.log(error)
-    if (error == "") {
-        error = {}
-    }
+
     data = {
         "status": status,
         "response": response,
