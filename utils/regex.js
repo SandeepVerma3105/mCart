8c0c19b53ca2616494f1d980a060c01dafa043d1
SandeepVerma3105 <sandeep.verma@appventurez.com>
2022-04-14 11:05:31 +0530

    regexx

--- utils/regex.js
+++ utils/regex.js
@@ -1,6 +1,6 @@
 module.exports = pattern = {
 
-    strPattern: /^([A-Z]+[a-zA-Z ]*([A-Z]*[a-z]+[ ])*[ ]+[0-9]*){2,100}$/,
+    strPattern: /^[A-Z]+[a-zA-Z ]*([A-Z]*[a-z]+[ ])*[ ]+[0-9]*$/,
     conturyCodePatter: /^(\+){1}[0-9]+$/,
     mobileNoPattern: /^[6-9][0-9]+$/,
     passwordPattern: /^[a-zA-Z0-9!@#\$%\^\&*_=+-]{8,12}$/,
